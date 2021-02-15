499fd60aa0cd90686218a03da7b4c4af3ee3794d
Akmhere <itzakm33@gmail.com>
2021-02-15 21:36:41 +0530

    third-commit

--- index.js
+++ index.js
@@ -4,11 +4,32 @@ $(".btm").click(function() {
     }, 50);
 });
   $(document).ready(function(){
-      console.log('hello');
+    //   console.log('hello');
       $('#myModal').modal('show')
   })
 
   $('.xyz').click(function(){
-    console.log('hello');
+    // console.log('hello');
     $('#myModal').modal('hide')
   })
+//   $('input').click(function(){
+//     console.log('helloww');
+//     $(this).addClass("main-border")
+//   })
+//   $(document).click(function(){
+//       $('input').removeClass("main-border");
+//   })
+jQuery(document).ready(function($) {
+
+    $("input").focus(function(){
+     
+      $(this).addClass("main-border");
+    //   console.log("add");
+   
+     }).blur(function(){
+          $(this).removeClass("main-border");
+        // console.log("remove");
+     })
+   
+   });    
+   
